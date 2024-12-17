f94d8084d3a4688bccf59c1ffd09cb265a6147e0
MehwishHanif <mehwish.hanif90@gmail.com>
2024-12-16 16:14:26 -0800

    routes setup and get all spots

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,7 +2,13 @@ const router = require('express').Router();
 
 const sessionRouter = require('./session.js');
 
-const useresRouter = require('./users.js');
+const usersRouter = require('./users.js');
+const spotsRouter = require('./spots.js');
+const reviewsRouter = require('./reviews.js');
+const spotImagesRouter = require('./spot-images.js');
+const reviewImagesRouter = require('./review-images.js');
+const bookingsRouter = require('./bookings.js');
+
 
 const { restoreUser } = require('../../utils/auth.js');
 
@@ -13,7 +19,13 @@ router.use(restoreUser);
 
 router.use('/session', sessionRouter);
 
-router.use('/users', useresRouter);
+router.use('/users', usersRouter);
+
+router.use('/spots', spotsRouter);
+router.use('/reviews', reviewsRouter);
+router.use('/spot-images', spotImagesRouter);
+router.use('/review-images', reviewImagesRouter);
+router.use('/bookings', bookingsRouter);
 
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
