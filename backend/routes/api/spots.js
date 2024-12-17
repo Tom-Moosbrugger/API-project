47664d86154a2b9ccaafc8194b29759083ef94ba
Tom-Moosbrugger <tamoosbrugger@gmail.com>
2024-12-17 09:06:43 -0800

    updated validation and error-handling for create a spot

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -11,6 +11,45 @@ const { Op, fn, col, ValidationError } = require('sequelize');
 
 const router = express.Router();
 
+const validateSpot = [
+    check('address')
+      .exists({ checkFalsy: true })
+      .withMessage('Address is required'),
+    check('city')
+      .exists({ checkFalsy: true })
+      .withMessage('City is required'),
+    check('state')
+      .exists({ checkFalsy: true })
+      .withMessage('State is required'),
+    check('country')
+      .exists({ checkFalsy: true })
+      .withMessage('Country is required'),
+    check('lat')
+      .exists({ checkFalsy: true })
+      .withMessage('lat is required')
+      .isInt({ min: -90, max: 90 })
+      .withMessage('Latitude must be within -90 and 90'),
+    check('lat')
+      .exists({ checkFalsy: true })
+      .withMessage('lat is required')
+      .isInt({ min: -180, max: 180 })
+      .withMessage('Longitude must be within -180 and 180'),
+    check('name')
+      .exists({ checkFalsy: true })
+      .withMessage('Name is required')
+      .isLength({ max: 49 })
+      .withMessage('Name must be less than 50 characters'),
+    check('description')
+      .exists({ checkFalsy: true })
+      .withMessage('Description is required'),
+    check('price')
+      .exists({ checkFalsy: true })
+      .withMessage('Price is required')
+      .isInt({ min: 0 })
+      .withMessage('Price per day must be a positive number'),
+    handleValidationErrors
+  ];
+
 router.get('/', async (req, res, next) => {
     const spots = await Spot.findAll( {
         include: [
@@ -39,23 +78,15 @@ router.get('/', async (req, res, next) => {
     });
 });
 
-router.post('/', requireAuth, async (req, res, next) => {
+router.post('/', requireAuth, validateSpot, async (req, res, next) => {
     const { user } = req;
 
-    try {
-        const newSpot = await Spot.create({
-            ownerId: user.id,
-            ...req.body
-        });
-        
-        res.status(201).json(newSpot);
-    } catch(err) {
-        if (err instanceof ValidationError) {
-            err.status = 400;
-        }
-
-        next(err);
-    }
+    const newSpot = await Spot.create({
+        ownerId: user.id,
+        ...req.body
+    });
+    
+    res.status(201).json(newSpot);
 });
 
 module.exports = router;
