c60322844716f26847e8a37080954ced4c5d44ae
MehwishHanif <mehwish.hanif90@gmail.com>
2024-12-18 14:12:49 -0800

    added get user bookings

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -7,12 +7,47 @@ const { handleValidationErrors } = require('../../utils/validation');
 const { requireAuth } = require('../../utils/auth');
 
 
-const { Spot, Review, SpotImage } = require('../../db/models');
-const { Op, fn, col } = require('sequelize');
+const { Spot, Review, Booking , SpotImage} = require('../../db/models');
+const { literal, Op, fn, col, ValidationError, where } = require('sequelize');
 
 
 const router = express.Router();
 
+router.get('/current', requireAuth, async (req, res, next) => {
+    const { user } = req;    
+    const userId = user.id;
 
+    const avgRatingSubquery = `(SELECT AVG(stars) 
+                           FROM Reviews 
+                           WHERE Reviews.spotId = Spot.id)`;
+
+    const previewImageSubquery = `(SELECT url
+                           FROM SpotImages 
+                           WHERE SpotImages.spotId = Spot.id 
+                           AND SpotImages.preview = true
+                           LIMIT 1)`;
+
+    const bookings = await Booking.findAll({
+      include: [
+        {
+          model: Spot,
+          attributes: {
+            include: [
+              [literal(avgRatingSubquery), "avgRating"],
+              [literal(previewImageSubquery), "previewImage"],
+            ],
+            exclude: ["description"], 
+          }
+        },
+      ],
+      where: {
+        userId,
+      },
+    });                       
+
+    res.status(200).json( {
+        Bookings: bookings
+    });
+});
 
 module.exports = router;
