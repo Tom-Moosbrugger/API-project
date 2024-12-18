3d8f886472844d552a7a77c8a72b44ccd89309da
Tom-Moosbrugger <tamoosbrugger@gmail.com>
2024-12-17 17:24:02 -0800

    updated get user reviews

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -37,7 +37,7 @@ router.get('/current', requireAuth, async (req, res, next) => {
             { 
                 model: Spot,
                 attributes: {
-                    exclude: ['createdAt', 'updatedAt'],
+                    exclude: ['description', 'createdAt', 'updatedAt'],
                 }
             },
             { 
